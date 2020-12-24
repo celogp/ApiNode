0f26497534c188bfce7cde027ad32302fb163ef2
Marcelo Gonçalves <celogp@gmail.com>
2020-12-23 21:25:37 -0300

    Update App.js
    Ajustando conexão

--- src/config/App.js
+++ src/config/App.js
@@ -1,5 +1,5 @@
 const DATABASE = require('./Db');
-const STRCONNECTION = 'mongodb+srv://' + DATABASE.US + ':' + DATABASE.PW +'@cluster0-j8xmk.mongodb.net/' + DATABASE.DB + '?retryWrites=true';
+
 const PORT = process.env.PORT || 3000;
 const LIMITLINHAS = DATABASE.LIMITLINHAS;
 
